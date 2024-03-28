2f82697d27b17b351a0de59149403a8aad0ea54b
Tan Heng <hengjt1@gmail.com>
2024-03-27 22:05:45 -0300

    Add Migration

--- src/PS.Produc.API/Controllers/AppDbContext.cs
+++ src/PS.Produc.API/Controllers/AppDbContext.cs
@@ -9,5 +9,50 @@ namespace PS.Produc.API.Controllers
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Category
+            modelBuilder.Entity<Category>()
+                .HasKey(c => c.CategoryId);
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            //Product
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Description)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.ImageURL)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(12,2)");
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    new Category { CategoryId = 1, Name = "Electronics" },
+                     new Category { CategoryId = 2, Name = "Clothes" });
+        }
     }
 }
---
+++ src/PS.Produc.API/Migrations/20240328005727_SeedProducts.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PS.Produc.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //insert data on products table
+            migrationBuilder
+                .Sql("INSERT INTO Products (Name, Description, ImageURL, Price, CategoryId, Stock)" +
+                    " VALUES ('Iphone 12', " +
+                    "'Iphone 12 128GB', " +
+                    "'https://www.iphonelife.com/sites/iphonelife.com/files/styles/panopoly_image_original/public/iphone-12-pro-max.jpg', " +
+                    "1000, " +
+                    "1," +
+                    "10)");
+
+            //insert data on products table with category 2
+            migrationBuilder
+                .Sql("INSERT INTO Products (Name, Description, ImageURL, Price, CategoryId, Stock)" +
+                    " VALUES ('T-Shirt', " +
+                    "'T-Shirt Black', " +
+                    "'https://www.iphonelife.com/sites/iphonelife.com/files/styles/panopoly_image_original/public/iphone-12-pro-max.jpg', " +
+                    "20, " +
+                    "2," +
+                    "5)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //Delete data from products table
+            migrationBuilder
+                .Sql("DELETE FROM Products");
+        }
+    }
+}
