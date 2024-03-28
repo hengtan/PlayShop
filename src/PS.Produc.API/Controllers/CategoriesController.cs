886a925e708b01463cc692c4613426c2761fbfe2
Tan Heng <hengjt1@gmail.com>
2024-03-28 02:34:24 -0300

    Add Controllers

---
+++ src/PS.Produc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.Produc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
